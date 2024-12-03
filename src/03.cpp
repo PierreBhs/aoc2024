ab24c78d53bdbbe75465b78ef14f42fe9a31213f
PierreBhs <pierre@pierre.com>
2024-12-03 17:38:29 +0100

    Add version without regex

--- src/03.cpp
+++ src/03.cpp
@@ -1,4 +1,5 @@
-#include <iostream>
+#include <cstdint>
+#include <fstream>
 #include <print>
 #include <regex>
 
@@ -7,8 +8,9 @@
 
 auto read_input()
 {
-    std::string str{""}, line;
-    while (std::getline(std::cin, line) && !line.empty()) {
+    std::ifstream input_file{"input/03.txt"};
+    std::string   str{""}, line;
+    while (std::getline(input_file, line) && !line.empty()) {
         str += line;
     }
     return str;
@@ -16,11 +18,10 @@ auto read_input()
 
 auto mult(std::string_view str)
 {
-    constexpr auto first_num_pos{4ul};
-    int            a{}, b{};
-    auto           comma_pos{str.find(',', first_num_pos)}, parenthesis_pos{str.find(')', comma_pos + 1)};
-    (void)std::from_chars(str.data() + first_num_pos, str.data() + comma_pos, a);
-    (void)std::from_chars(str.data() + comma_pos + 1, str.data() + (parenthesis_pos - comma_pos), b);
+    int  a{}, b{};
+    auto comma_pos{str.find(',')};
+    (void)std::from_chars(str.data(), str.data() + comma_pos, a);
+    (void)std::from_chars(str.data() + comma_pos + 1, str.data() + str.size(), b);
     return a * b;
 }
 
@@ -33,10 +34,44 @@ auto part1(const std::string& input)
 
     auto sum{0};
     while (std::regex_search(start, end, match, regex)) {
-        sum += mult(match[0].str());
+        sum += mult((match[0].str().c_str() + 4));
         start = match.suffix().first;
     }
 
+    return sum;
+}
+
+auto check_valid(char c)
+{
+    return std::isdigit(c) || c == ',';
+}
+
+auto part1_no_regex(const std::string& input)
+{
+    auto sum{0};
+    auto i{0ul};
+
+    while ((i = input.find("mul(", i)) != std::string::npos) {
+        i += 4;  // skip mul(
+
+        std::string substr{""};
+        bool        do_mult = true;
+        while (input[i] != ')') {
+            if (check_valid(input[i])) {
+                substr += input[i];
+                i++;
+            } else {
+                do_mult = false;
+                break;
+            }
+        }
+
+        if (do_mult && std::count(substr.begin(), substr.end(), ',') == 1) {
+            sum += mult(substr);
+        }
+
+        i++;
+    }
     return sum;
 }
 
@@ -57,9 +92,57 @@ auto part2(const std::string& input)
         }
 
         if (todo) {
-            sum += mult(match[0].str());
+            sum += mult(match[0].str().c_str() + 4);
         }
         start = match.suffix().first;
+    }
+
+    return sum;
+}
+
+auto part2_no_regex(const std::string& input)
+{
+    auto sum{0};
+    for (auto i{0ul}; i < input.size(); i++) {
+        if ((i = input.find("mul(", i)) == std::string::npos) {
+            break;
+        }
+        auto do_pos{input.rfind("do()", i)};
+        auto dont_pos{input.rfind("don't()", i)};
+
+        if (do_pos == std::string::npos) {
+            do_pos = dont_pos - 1;
+        }
+        if (dont_pos == std::string::npos) {
+            dont_pos = do_pos - 1;
+        }
+
+        auto do_dist{std::abs(static_cast<std::int32_t>(i - do_pos))},
+            dont_dist{std::abs(static_cast<std::int32_t>(i - dont_pos))};
+        auto do_dist_min = (std::min(do_dist, dont_dist) == do_dist) ? true : false;
+
+        i += 4;
+
+        if (!do_dist_min) {
+            // skip to next do() directly if we're in a don't
+            i = input.find("do()", i) - 1;
+        }
+
+        std::string substr{""};
+        bool        do_mult = true;
+        while (input[i] != ')') {
+            if (check_valid(input[i])) {
+                substr += input[i];
+                i++;
+            } else {
+                do_mult = false;
+                break;
+            }
+        }
+
+        if (do_mult && std::count(substr.begin(), substr.end(), ',') == 1) {
+            sum += mult(substr);
+        }
     }
 
     return sum;
@@ -71,22 +154,41 @@ int main()
 
     auto begin{std::chrono::steady_clock::now()};
 
-    auto safe_reports{part1(input)};
+    auto mult_regex{part1(input)};
 
     auto end{std::chrono::steady_clock::now()};
-    std::println("Mult res: {} | time = {}us",
-                 safe_reports,
+    std::println("Regex - Mult res: {} | time = {}us",
+                 mult_regex,
                  std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count());
 
     begin = std::chrono::steady_clock::now();
 
-    auto safe_reports_dampener{part2(input)};
+    auto mult{part1_no_regex(input)};
+
+    end = std::chrono::steady_clock::now();
+    std::println(
+        "Mult res: {} | time = {}us", mult, std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count());
+
+    begin = std::chrono::steady_clock::now();
+
+    auto do_mult_regex{part2(input)};
+
+    end = std::chrono::steady_clock::now();
+    std::println("Regex - Mult res with do's: {} | time = {}us",
+                 do_mult_regex,
+                 std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count());
+
+    begin = std::chrono::steady_clock::now();
+
+    auto do_mult{part2_no_regex(input)};
 
     end = std::chrono::steady_clock::now();
     std::println("Mult res with do's: {} | time = {}us",
-                 safe_reports_dampener,
+                 do_mult,
                  std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count());
 }
 
-// Mult res: 173517243 | time = 264us
-// Mult res with do's: 100450138 | time = 294us
+// Regex - Mult res: 173517243 | time = 230us
+// Mult res: 173517243 | time = 16us
+// Regex - Mult res with do's: 100450138 | time = 305us
+// Mult res with do's: 100450138 | time = 125us
